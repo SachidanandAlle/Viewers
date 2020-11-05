bebd8c4eaa9507ebb66fc9a90d2f49d9865336cc
Sachidanand Alle <salle@nvidia.com>
2020-11-05 06:15:04 -0800

    Support other toolbars

--- extensions/nvidia-aiaa/src/init.js
+++ extensions/nvidia-aiaa/src/init.js
@@ -14,7 +14,44 @@ export default function init({ servicesManager, configuration }) {
   console.info(DeepgrowProbeTool);
   console.info(DExtr3DProbeTool);
 
-  const { CircleScissorsTool, FreehandScissorsTool, RectangleScissorsTool } = csTools;
-  const tools = [DeepgrowProbeTool, DExtr3DProbeTool, CircleScissorsTool, FreehandScissorsTool, RectangleScissorsTool];
+  const {
+    BrushTool,
+    SphericalBrushTool,
+    CorrectionScissorsTool,
+    CircleScissorsTool,
+    FreehandScissorsTool,
+    RectangleScissorsTool
+  } = csTools;
+
+  const tools = [
+    DeepgrowProbeTool,
+    DExtr3DProbeTool,
+    BrushTool,
+    SphericalBrushTool,
+    CorrectionScissorsTool,
+    CircleScissorsTool,
+    FreehandScissorsTool,
+    RectangleScissorsTool
+  ];
+
   tools.forEach(tool => csTools.addTool(tool));
+
+  csTools.addTool(BrushTool, {
+    name: 'BrushEraser',
+    configuration: {
+      alwaysEraseOnClick: true,
+    },
+  });
+  csTools.addTool(CircleScissorsTool, {
+    name: 'CircleScissorsEraser',
+    defaultStrategy: 'ERASE_INSIDE',
+  });
+  csTools.addTool(FreehandScissorsTool, {
+    name: 'FreehandScissorsEraser',
+    defaultStrategy: 'ERASE_INSIDE',
+  });
+  csTools.addTool(RectangleScissorsTool, {
+    name: 'RectangleScissorsEraser',
+    defaultStrategy: 'ERASE_INSIDE',
+  });
 }
